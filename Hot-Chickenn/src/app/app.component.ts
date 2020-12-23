82f741cfdb69ab0b708a9d205605d4220d22d7dd
juninhoww2 <alqadianokira@gmail.com>
2020-12-23 19:02:39 -0300

    splashScreen

--- Hot-Chickenn/src/app/app.component.ts
+++ Hot-Chickenn/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
@@ -12,7 +10,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
@@ -20,8 +17,7 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this.statusBar.styleDefault();  
     });
   }
 }
