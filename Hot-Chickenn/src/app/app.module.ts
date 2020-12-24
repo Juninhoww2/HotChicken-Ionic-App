96339ec3b4110d478adb3625da6da831556607a8
juninhoww2 <alqadianokira@gmail.com>
2020-12-24 02:25:26 -0300

    addBackendServices

--- Hot-Chickenn/src/app/app.module.ts
+++ Hot-Chickenn/src/app/app.module.ts
@@ -9,10 +9,21 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+     IonicModule.forRoot(),
+     AppRoutingModule,
+     AngularFireModule.initializeApp(environment.firebaseConfig),
+     AngularFirestoreModule,
+     AngularFireStorageModule],
   providers: [
     StatusBar,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- Hot-Chickenn/src/environments/environment.ts
+++ Hot-Chickenn/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD4WTMIXEXqY9KrfRTwB-0At1G4GW0XQH0",
+    authDomain: "hotchickenpg.firebaseapp.com",
+    databaseURL: "https://hotchickenpg-default-rtdb.firebaseio.com",
+    projectId: "hotchickenpg",
+    storageBucket: "hotchickenpg.appspot.com",
+    messagingSenderId: "608314830790",
+    appId: "1:608314830790:web:82310c2247cabb4ff90cc7"
+  }
 };
 
 /*
